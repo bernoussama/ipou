a767465dc67b3d650c4534c8566209723a438b2f
bernoussama <oussama.bernou@outlook.com>
2025-07-19 22:56:16 +0100

    add ip route to run script

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ sudo setcap cap_net_admin=eip target/release/ipou
 target/release/ipou &
 pid=$!
 # sudo ip addr add 10.0.0.1/24 dev utun0
-# sudo ip route add 10.0.0.0/8 dev utun0
+sudo ip route add 10.0.0.0/8 dev utun0
 # sudo ip link set up dev utun0
 trap "kill $pid" INT TERM
 wait $pid
