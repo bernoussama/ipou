400ed0471e7094c61e38de721028e117234e2b05
bernoussama <oussama.bernou@outlook.com>
2025-07-23 18:57:51 +0100

    add unknown type

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,7 @@
 #[derive(thiserror::Error, Debug)]
-pub enum IpouError {}
+pub enum IpouError {
+    #[error("An unknown error occurred: {0}")]
+    Unknown(String),
+}
 
 pub type Result<T> = std::result::Result<T, IpouError>;
